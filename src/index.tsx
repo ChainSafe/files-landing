5435667114000857cacddbcf317ca56dd761258b
Thibaut Sardan <33178835+Tbaut@users.noreply.github.com>
2021-09-17 13:21:28 +0100

    don't track users before they opt-in (#22)

--- src/index.tsx
+++ src/index.tsx
@@ -6,8 +6,10 @@ import posthog from "posthog-js"
 if (process.env.REACT_APP_POSTHOG_PROJECT_API_KEY &&
   process.env.REACT_APP_POSTHOG_INSTANCE_ADDRESS) {
   posthog.init(process.env.REACT_APP_POSTHOG_PROJECT_API_KEY, {
-    api_host: process.env.REACT_APP_POSTHOG_INSTANCE_ADDRESS
-  })
+    api_host: process.env.REACT_APP_POSTHOG_INSTANCE_ADDRESS,
+    opt_out_capturing_by_default: true
+  },
+  )
 }
 
 ReactDOM.render(
